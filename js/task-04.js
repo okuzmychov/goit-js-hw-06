3f71f3089c739dfc4034df93fd767aa3ec06f951
okuzmychov <o.kuzmichev@gmail.com>
2023-06-17 23:00:32 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,17 @@
-const selectors = {
-    decrement: document.querySelector('#decrement'),
-    increment: document.querySelector('#increment'),
-}    
+const valueElement = document.getElementById('value');
+const decrementButton = document.querySelector('button[data-action="decrement"]');
+const incrementButton = document.querySelector('button[data-action="increment"]');
+let counterValue = 0;
+
+ const incrementCounter = () => {
+    counterValue += 1;
+    valueElement.textContent = counterValue;
+  };
+
+  const decrementCounter = () => {
+    counterValue -= 1;
+    valueElement.textContent = counterValue;
+  };
+
+  incrementButton.addEventListener('click', incrementCounter);
+  decrementButton.addEventListener('click', decrementCounter);
